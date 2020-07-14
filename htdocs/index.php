f6e340819c16e67e7defd126bf721959f82aba53
CliffLam760 <64488017+CliffLam760@users.noreply.github.com>
2020-07-15 01:32:29 +0800

    Add files via upload

--- htdocs/index.php
+++ htdocs/index.php
@@ -15,5 +15,22 @@ include("mysql_connect.inc.php");
 <body>
   <h2>Welcome Team27!</h2>
 </body>
+
+<form action="list_overseas.php" method="post">
+<input type="submit" value="list oversea"><br>
+</form>
+
+<form action="list_106_student.php" method="post">
+<input type="submit" value="list 106"><br>
+</form>
+
+<form action="list_107_student.php" method="post">
+<input type="submit" value="list 107"><br>
+</form>
+
+<form action="list_108_student.php" method="post">
+<input type="submit" value="list 108"><br>
+</form>
+
 </html>
 
---
+++ htdocs/list_overseas.php
@@ -0,0 +1,31 @@
+<?php session_start(); ?>
+
+<input type="button" onclick="history.back()" value="back" ><br>
+<?php
+include("mysql_connect.inc.php");
+
+$max = 11;
+
+$data = $mysqli->query("select * from overseas");
+?>
+<?php $row = $data->fetch_fields(); ?>
+<table border="1">
+<tr>
+<?php for($i=0;$i<$max;$i++)
+{
+?>
+<td><?php echo $row[$i]->name ?></td>
+<?php }?>
+</tr>
+<?php while ($row = $data->fetch_row()): ?>
+<tr>
+<?php for($i=0;$i<$max;$i++)
+{
+?>
+<td><?php echo $row[$i] ?></td>
+<?php }?>
+</tr>
+<?php endwhile ?>
+
+
+</table>
